b02fc3f918b946c627847d5f63b575c92b30c7ea
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-28 14:43:46 +0530

    Add files via upload

---
+++ 28-09-22(AssignmentDay2)/RectangleTest.java
@@ -0,0 +1,12 @@
+package com.Assignment1;
+
+public class RectangleTest {
+  public static void main(String[] args) {
+	  Rectangle obj1 = new Rectangle();
+	  Rectangle obj2 = new Rectangle(20,25);
+	  
+	  System.out.println("Area = " +obj1.getArea());
+	  System.out.println("New Area = " +obj2.getArea());
+
+}
+}
