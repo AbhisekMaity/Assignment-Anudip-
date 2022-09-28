a1e93b164e8471957fa0eddc6c9eb91b2f3f249c
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-28 14:03:12 +0530

    Add files via upload

---
+++ 28-09-22(AssignmentDay2)/RectangleTest.java
@@ -0,0 +1,8 @@
+package com.Assignment1;
+
+public class RectangleTest {
+  public static void main(String[] args) {
+	  Rectangle obj1 = new Rectangle();
+	  Rectangle obj2 = new Rectangle(20,25);
+}
+}
