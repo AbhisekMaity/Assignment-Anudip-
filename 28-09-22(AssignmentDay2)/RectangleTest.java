90204c6f7d8497188cbe8c2c0e2e5c9e6041339b
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-28 14:43:32 +0530

    Delete RectangleTest.java

--- 28-09-22(AssignmentDay2)/RectangleTest.java
+++
@@ -1,8 +0,0 @@
-package com.Assignment1;
-
-public class RectangleTest {
-  public static void main(String[] args) {
-	  Rectangle obj1 = new Rectangle();
-	  Rectangle obj2 = new Rectangle(20,25);
-}
-}
